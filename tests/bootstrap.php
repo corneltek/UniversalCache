1c12d7d93a4d13ae725b99836b5d460e7ab92707
c9s <cornelius.howl@gmail.com>
2013-03-23 23:55:34 +0800

    Fix composer.json for require-dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,2 @@
 <?php
-require 'PHPUnit/TestMore.php';
-require 'Universal/ClassLoader/SplClassLoader.php';
-$loader = new \Universal\ClassLoader\SplClassLoader( array(  
-    'UniversalCache' => 'src'
-));
-$loader->useIncludePath(true);
-$loader->register();
+require "vendor/autoload.php";
