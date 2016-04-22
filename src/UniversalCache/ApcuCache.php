b98a389787557e705d091b5f8e896993f64ebffd
c9s <yoanlin93@gmail.com>
2016-04-23 00:04:24 +0800

    Add ApcuCache support

---
+++ src/UniversalCache/ApcuCache.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * This file is part of the UniversalCache package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace UniversalCache;
+
+class ApcuCache
+{
+    public $namespace = '';
+
+    public $defaultExpiry = 0;
+
+    public function __construct( $options = array() )
+    {
+        if ( isset($options['namespace']) ) {
+            $this->namespace = $options['namespace'];
+        }
+        if ( isset($options['default_expiry'] ) ) {
+            $this->defaultExpiry = $options['default_expiry'];
+        }
+    }
+
+    public function get($key)
+    {
+        return apcu_fetch( $this->namespace . ':' . $key );
+    }
+
+    public function set($key,$value,$ttl = null)
+    {
+        if( null === $ttl && $this->defaultExpiry )
+            $ttl = $this->defaultExpiry;
+        apcu_store( $this->namespace . ':' . $key , $value , $ttl );
+    }
+
+    public function remove($key)
+    {
+        apcu_delete( $this->namespace . ':' . $key );
+    }
+
+    public function clear()
+    {
+        apcu_clear_cache();
+    }
+
+    public function __get($name)
+    {
+        return $this->get($name);
+    }
+
+    public function __set($name,$value)
+    {
+        $this->set($name,$value);
+    }
+
+    static function getInstance()
+    {
+        static $instance;
+        return $instance ? $instance : $instance = new static;
+    }
+
+}
+
+
