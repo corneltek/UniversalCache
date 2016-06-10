3c79d2093a9aefc3abebf0877a2253b4420bdd36
c9s <yoanlin93@gmail.com>
2016-06-10 16:27:38 +0800

    Drop serializer support

--- tests/UniversalCache/ApcCacheTest.php
+++ tests/UniversalCache/ApcCacheTest.php
@@ -4,6 +4,9 @@ class ApcCacheTest extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
+        if (version_compare(phpversion(),"5.5") > 0) {
+            $this->markTestSkipped("apc test is only for php 5.3 and 5.4");
+        }
         $cache = new UniversalCache\ApcCache('app_');
         $cache->set('key','val1');
         $cache->get('key');
--- tests/UniversalCache/FileSystemCacheTest.php
+++ tests/UniversalCache/FileSystemCacheTest.php
@@ -3,31 +3,6 @@ use UniversalCache\FileSystemCache;
 
 class FileSystemCacheTest extends PHPUnit_Framework_TestCase
 {
-
-    public function testWithJsonSerializer()
-    {
-        $serializer = new SerializerKit\Serializer('json');
-        $cache = new UniversalCache\FileSystemCache('cache', array( 
-            'serializer' => $serializer,
-        ));
-        $cache->clear();
-
-        $url = 'foo_bar';
-        $html = 'test content';
-        $cache->set( $url , $html );
-        $html2 = $cache->get( $url );
-
-        is( $html , $html2 );
-
-        $cache->remove( $url );
-
-        ok( null === $cache->get( $url ) );
-
-        $cache->clear();
-
-        ok( null === $cache->get( $url ) );
-    }
-
     public function test()
     {
         $cache = new UniversalCache\FileSystemCache('cache', array( 
